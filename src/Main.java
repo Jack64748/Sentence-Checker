abc6295f9c2d33e6d301c57eb44cf96d78a098e5
Jack64748 <Brady-J17@ulster.ac.uk>
2023-12-17 13:29:22 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your sentence");
+        String sentence = scanner.nextLine();
+        System.out.println("Sentence is: "+sentence);
+    }
+}
